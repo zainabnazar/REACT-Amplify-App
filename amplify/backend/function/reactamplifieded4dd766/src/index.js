17dbb9b6bf51544db4d6616fa2f21d4a21765f3d
zainabnoori <zainab.noori@and.digital>
2022-11-29 12:38:09 +0000

    Re do the api and lambda creation

---
+++ amplify/backend/function/reactamplifieded4dd766/src/index.js
@@ -0,0 +1,21 @@
+
+// /**
+//  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+//  */
+exports.handler = async (event) => {
+    const itemId = event.pathParameters.itemId;
+    const item = {
+        "itemId": itemId,
+        "item": "Item " + itemId
+    }
+    console.log("itemId: " + itemId);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify(item),
+    };
+    return response;
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "reactamplifieded4dd766": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "reactAmplify": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}

3063c7ba0e28fc4fcbe425262af3fe6d0bddc3d7
zainabnoori <zainab.noori@and.digital>
2022-12-01 12:50:59 +0000

    WTest getting 200 status when we call our api

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('Should have the correct text', async () => {
   const myApp = await screen.findByText('React App Using Amplify');
   expect(myApp).toBeVisible();
 });
+
+test('We get 200 status when we make the GET call', async () => {
+  let response = await fetch("https://0tipw7uf5j.execute-api.eu-west-2.amazonaws.com/dev/items/1");
+  expect(response.status).toBe(200);
+
+})

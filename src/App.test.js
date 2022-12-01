6236c6a798b64c005b1c9e7302bb94848c64d712
zainabnoori <zainab.noori@and.digital>
2022-12-01 12:57:44 +0000

    Cover the 4XX status for failing request

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,13 @@ test('Should have the correct text', async () => {
 });
 
 test('We get 200 status when we make the GET call', async () => {
-  let response = await fetch("https://0tipw7uf5j.execute-api.eu-west-2.amazonaws.com/dev/items/1");
+  let response = await fetch("https://0tipw7uf5j.execute-api.eu-west-2.amazonaws.com/dev/items/mobile");
   expect(response.status).toBe(200);
 
+})
+
+test("Get 4XX status when we are not passing the item in the request", async () => {
+  let response = await fetch("https://0tipw7uf5j.execute-api.eu-west-2.amazonaws.com/dev/items/");
+  console.log("!!!!!!!!!!!", response.status);
+  expect(response.status).toBe(403);
 })
